0e634eebf7aa00173c87ab426b880146696131cb
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2012-07-21 02:14:18 -0700

    Different stuffs

--- test.rb
+++ test.rb
@@ -3,10 +3,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), "linky_list"))
 def test_with(n)
   l = Linky::List.new
   (1..n).each {|i| l << i}
-  l.each {|i| puts i}
+  # l.each {|i| puts i}
 end
 
-[1, 10, 100, 1000, 10000].each do |i|
+[1, 10, 100, 1000, 10000, 100000].each do |i|
   puts "Testing with #{i}..."
   sleep 3
   test_with i
