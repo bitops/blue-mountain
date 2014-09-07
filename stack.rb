be41cce494e894342e8983c7a13888528da6dd0e
Sebastian Wittenkamp <sebastian@bitops.io>
2014-09-07 15:54:25 -0700

    Simple Stack implementation

---
+++ stack.rb
@@ -0,0 +1,14 @@
+class Stack
+  def initialize
+    @values = []
+  end
+
+  def push(value)
+    @values << value
+    value
+  end
+
+  def pop
+    @values.delete_at(@values.length - 1)
+  end
+end
